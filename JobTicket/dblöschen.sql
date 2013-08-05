1341d7dd2f0e33b1929e9c3bc895ba87a58760c0
Jan <mueller.jan.87@gmail.com>
2013-08-05 18:20:21 +0200

    SQL-Skript zum Löschen der Datenbank erstellt

---
+++ JobTicket/dblöschen.sql
@@ -0,0 +1,7 @@
+drop table JOBTICKET.jobbearbeiter;
+drop table JOBTICKET.kosten; 
+drop table JOBTICKET.angestellte;
+drop table JOBTICKET.angestelltenbezeichnungen;
+drop table JOBTICKET.produkteigenschaften;
+drop table JOBTICKET.jobs;
+drop table JOBTICKET.kunde;
